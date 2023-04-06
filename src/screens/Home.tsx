de61341e4215b94937ccd7a19342ce5d4e8a7b84
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-04-05 21:47:42 -0300

    feat: Finish home screen interface

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,29 +1,72 @@
-import { HStack, VStack } from 'native-base'
+import { useState } from 'react'
+import { FlatList, Heading, HStack, VStack, Text } from 'native-base'
 
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
-import { useState } from 'react'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'ombro', 'Bíceps', 'tríceps'])
   const [groupSelected, setGroupSelected] = useState('costa')
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento curvado'])
 
   return (
     <VStack flex={1}>
 
       <HomeHeader />
 
-      <HStack>
-        <Group
-          name='costa'
-          isActive={groupSelected === 'costa'}
-          onPress={() => setGroupSelected('costa')}
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack
+        flex={1}
+        px={8}
+      >
+
+        <HStack
+          justifyContent={'space-between'}
+          mb={5}
+        >
+
+          <Heading
+            color={'gray.200'}
+            fontSize={'md'}
+          >
+            Exercícios
+          </Heading>
+
+          <Text
+            color={'gray.200'}
+            fontSize={'sm'}
+          >
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
         />
-        <Group
-          name='ombro'
-          isActive={groupSelected === 'ombro'}
-          onPress={() => setGroupSelected('ombro')}
-        />
-      </HStack>
+      </VStack>
     </VStack>
   )
 }
