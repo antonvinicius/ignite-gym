25f71ddd4cb51004da01af0619249c5a792905ac
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-04-05 21:13:55 -0300

    feat: Add groups on home

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,51 @@
+import { TouchableOpacity } from "react-native";
+import { Heading, HStack, VStack, Text, Icon } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { UserPhoto } from "@components/UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack
+      bg={'gray.600'}
+      pt={16}
+      px={8}
+      pb={5}
+      alignItems={'center'}
+    >
+
+      <UserPhoto
+        source={{ uri: 'https://github.com/antonvinicius.png' }}
+        size={16}
+        alt={'Foto de perfil do usuário'}
+        mr={4}
+      />
+
+      <VStack flex={1}>
+
+        <Text
+          color={'gray.100'}
+          fontSize={'md'}
+        >
+          Olá,
+        </Text>
+
+        <Heading
+          color={'gray.100'}
+          fontSize={'md'}
+        >
+          Vinícius
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name={'logout'}
+          color={'gray.200'}
+          size={7}
+        />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,29 @@
-import { Center, Text } from 'native-base'
+import { HStack, VStack } from 'native-base'
+
+import { HomeHeader } from '@components/HomeHeader'
+import { Group } from '@components/Group'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
-    <Center flex={1}>
-      <Text color={'white'}>Home</Text>
-    </Center>
+    <VStack flex={1}>
+
+      <HomeHeader />
+
+      <HStack>
+        <Group
+          name='costa'
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group
+          name='ombro'
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
+    </VStack>
   )
 }
