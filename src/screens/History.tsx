eed4792ba48cc125ab59eb246ac53bc4a6823a0b
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-04-07 12:02:23 -0300

    feat: Finish history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,59 @@
-import { Center, Text } from 'native-base'
+import { useState } from 'react'
+import { Center, Heading, Text, VStack, SectionList } from 'native-base'
+
+import { ScreenHeader } from '@components/ScreenHeader'
+import { HistoryCard } from '@components/HistoryCard'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.2020',
+      data: ['Puxada frontal', 'Remada frontal']
+    },
+    {
+      title: '27.08.2020',
+      data: ['Rosca cross', 'Rosca martelo']
+    }
+  ])
+
   return (
-    <Center flex={1}>
-      <Text color={'white'}>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color={'gray.100'}
+            fontSize={'md'}
+            mt={10}
+            mb={3}
+          >
+            {section.title}
+          </Heading>
+        )}
+        ListEmptyComponent={() => (
+          <Text
+            color={'gray.100'}
+            textAlign={'center'}
+          >
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && {
+            flex: 1,
+            justifyContent: 'center'
+          }
+        }
+        showsVerticalScrollIndicator={false}
+        px={8}
+      />
+    </VStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,7 +7,7 @@ import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
   const [groups, setGroups] = useState(['Costas', 'ombro', 'Bíceps', 'tríceps'])
-  const [groupSelected, setGroupSelected] = useState('costa')
+  const [groupSelected, setGroupSelected] = useState('Costas')
   const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento curvado'])
 
   return (
@@ -21,7 +21,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={String(groupSelected).toLocaleUpperCase() === String(item).toLocaleUpperCase()}
             onPress={() => setGroupSelected(item)}
           />
         )}
